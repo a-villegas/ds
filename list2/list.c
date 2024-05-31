83446e4bd868f1855e1b7fae76d20c2249dac81d
Amauri Villegas <116932334+a-villegas@users.noreply.github.com>
2024-05-31 02:37:17 -0400

    Added list2 directory to hold my solution to Stanford's CS Class LinkedList problems.

---
+++ list2/list.c
@@ -0,0 +1,283 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+const int LIST_NODE_SIZE = 3;
+
+struct node {
+    int data;
+    struct node * next;
+};
+
+int Length(struct node * head);
+struct node * createNode(int _data);
+struct node * BuildOneTwoThree();
+void BasicsCaller();
+void printList(struct node * List);
+void Push(struct node ** headRef, int newData);
+int addAtTail(struct node * head, int _data);
+int Count(struct node * list, int key);
+void GetNthTest();
+int GetNth(struct node * list, int position);
+
+int main(int argc, char * argv[])
+{
+    BasicsCaller();
+    return EXIT_SUCCESS;
+}
+
+/***************************************************************************
+*   Searches for the number of iterations a key has in the list:
+*   @param list: the head of te list
+*   @param key:  element to look for in the list 
+*   @return: On success returns the number of iterations of key in the list
+             On failure returns 0.
+*****************************************************************************/
+void BasicsCaller()
+{
+    struct node * head;
+    int key = 69;
+    int len;
+
+    head = BuildOneTwoThree(); // Start with {1, 2, 3}
+    Push(&head, 13); // Push 13 on the front, yielding {13, 1, 2, 3}
+                     // (THe '&' is because head is passed as a reference pointer.)
+    Push(&(head->next), 42); //Push 42 into the second position {13, 42 , 1, 2 ,3}
+    addAtTail(head, key);
+    Push(&(head->next->next), key);
+    Push(&head, key);
+    len = Length(head);  //Computes the length = 6;
+    printf("List size: %d\n",len);
+    printList(head);
+    printf("key %d: occurs %d times\n", key, Count(head, key));
+
+    GetNthTest();
+}
+
+void GetNthTest()
+{
+    int key = 3;
+    struct node * mylist = BuildOneTwoThree();
+    printf("List size: %d\n", Length(mylist));
+    printList(mylist);
+    printf("key %d: occurs %d times\n", key, Count(mylist, key));
+    if(mylist) {
+        int lastNode = GetNth(mylist, 2);
+        printf("GetNth Element : %d\n",lastNode);
+
+        lastNode = GetNth(mylist, 0);
+        printf("GetNth Element : %d\n",lastNode);
+
+        //lastNode = GetNth(mylist, -1);
+        //printf("GetNth Element : %d\n",lastNode);
+
+        //lastNode = GetNth(mylist, (Length(mylist) + 1));
+        //printf("GetNth Element : %d\n",lastNode);
+    }
+}
+
+int GetNth(struct node * list, int position)
+{
+    int element = -9999;
+    //assert(position > 0 || position < (Length(list) - 1));
+    if( position < 0 || position > (Length(list) - 1)) {
+        printf("Error: Program Asserted!!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if(!list) {
+        goto Error;
+    }
+
+    struct node * current = list;
+    while(current) {
+        if(position == 0){
+            element = current->data;
+            break;
+        }
+        current = current->next;
+        position--;
+    }
+
+    Error:
+    return element;
+}
+
+/***************************************************************************
+*   Searches for the number of iterations a key has in the list:
+*   @param list: the head of te list
+*   @param key:  element to look for in the list 
+*   @return: On success returns the number of iterations of key in the list
+             On failure returns 0.
+*****************************************************************************/
+void printList(struct node * List)
+{
+    if(List) {
+        struct node * current = List;
+        printf("{ ");
+        while(current) {
+            printf("%d ", current->data);
+            current = current->next;
+        }
+        printf("}\n");
+    } else {
+        printf("NULL List\n");
+    }
+}
+
+/***************************************************************************
+*   Searches for the number of iterations a key has in the list:
+*   @param list: the head of te list
+*   @param key:  element to look for in the list 
+*   @return: On success returns the number of iterations of key in the list
+             On failure returns 0.
+*****************************************************************************/
+int Count(struct node * list, int key)
+{
+    int count = 0;
+    if(!list) {
+        printf("The list is empty\n");
+        goto Error;
+    }
+
+    struct node * current = list;
+    while(current) {
+        if(current->data == key){
+            count++;
+        }
+        current = current->next;
+    }
+    Error:
+    return count;
+}
+
+/***************************************************************************
+*   Adds an element at the end of the list:
+*   @param head: the head of te list
+*   @param data: data to add to list 
+*   @return: On success return 1, on failure return 0
+*****************************************************************************/
+int addAtTail(struct node * head, int _data)
+{
+    int SUCCESS = 0;
+    if(!head) { 
+        printf("Something went wrong\n");
+        goto Error;
+    }
+    
+    struct node * newElem = createNode(_data);
+    if(!newElem) {
+        printf("Something went wrong\n");
+        goto Error;
+    }
+
+    struct node * tail = head;
+    while(tail) {
+        if(!tail->next) {
+            
+            tail->next = newElem;
+            break;
+        }
+        tail = tail->next;
+    }
+    SUCCESS = 1;
+
+    Error:
+    return SUCCESS;
+}
+
+/*************************************************************************
+*
+*   Returns the number of nodes in the list 
+*   @param head: pointer to the list we need to get the size of
+*   @return: Returns the number of elements in the list, zero if empty
+*
+***************************************************************************/
+int Length(struct node * head)
+{
+    int size = 0;
+    if(!head) {
+        goto Error;
+    }
+
+    struct node * current = head;
+    while(current != NULL) {
+        if(current) {
+            size++;
+        }
+        current = current->next;
+    }
+
+
+    Error:
+    return size;
+}
+
+/*************************************************************************************************
+*
+*  Function creates a Node and initializes the data
+*  @param _data:  Element data for a node list
+*  @return: On success returns a struct node with its data field populated and next field = NULL
+*          On failure returns NULL
+*
+***************************************************************************************************/
+struct node * createNode(int _data)
+{
+    struct node * temp = (struct node *) malloc(sizeof(struct node));
+    if(!temp) {
+        printf("\nCouldn't allocate memory for a node!!!\n");
+        goto Error;
+    }
+
+    temp->data = _data;
+    temp->next = NULL;
+
+    Error:
+    return temp;
+}
+
+
+/************************************************************************************
+*
+*     Allocates and returns the list {1, 2, 3}. Used by some of the example code
+*     to build lists to work on
+*     @return: On success it returns list with elements {1,2,3}
+*              On failure returns NULL
+*
+************************************************************************************/
+struct node * BuildOneTwoThree()
+{
+    struct node * headList = NULL;
+    struct node * temp = NULL;
+    for( int index = 3; index > 0; index-- ) {
+        temp = createNode(index);
+        if(!temp) {
+            goto Error;
+        }
+
+        if(!headList) {
+            headList = temp;
+        } else {
+            temp->next = headList;
+            headList = temp;
+        }
+
+    }
+
+    return headList;
+
+    Error:
+    return NULL;
+}
+
+
+void Push(struct node ** headRef, int newData)
+{
+    if(headRef){
+        struct node * newNode = createNode(newData);
+        if(newNode){
+            newNode->next = *(headRef);
+            *(headRef) = newNode;
+        }
+    }
+}
